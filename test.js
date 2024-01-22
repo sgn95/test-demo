5078f84380dd2aa8762f2b80dde6b3794230dcd0
宋光暖 <30686269+sgn95@users.noreply.github.com>
2024-01-22 17:48:20 +0800

    master commit

--- test.js
+++ test.js
@@ -1,15 +1,1 @@
-console.log('test');
-console.log('success');
-console.log('master commit');
-console.log('dev commit');
-console.log('dev commit');
-
-
-
-
-console.log('master commit');
-console.log('dev commit');
-console.log('dev commit');
-console.log('dev commit');
-console.log('dev commit');
-console.log('dev commit1');
+console.log('master commit')
